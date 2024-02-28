dc8158a88b282fc2154756d6473b93eba1249a35
kwanwoo98 <kwoo6475@gmail.com>
2024-02-28 10:45:00 +0900

    first commit

---
+++ JavaProject240228/src/ex_240228/Test1.java
@@ -0,0 +1,18 @@
+package ex_240228;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// vscode에서 처럼, UI 폰트, 글자 크기, 자동완성 등.
+		// 자바스크립트 함수 이름
+		// 자바에서는 이름 = 메서드 로 사용
+		// 정의, 호출(사용)
+		// 역할: 콘솔에 메세지를 출력.
+		// 프로그래밍에서는 항상 값을 잘 입출력 확인 중요
+		System.out.println("Hellow World.!!");
+		
+		
+	}
+
+}
---
+++ JavaProject240228/src/javatest/javatest.java
@@ -0,0 +1,12 @@
+package javatest;
+
+public class javatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// syso ctrl + space 자동완성
+		System.out.println("Hellow world");
+		System.out.println();
+	}
+
+}
